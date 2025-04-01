62ef7efa1516bf578b7be4a199d8ce1c18c56657
william <williamlafrance5797@gmail.com>
2025-04-01 14:42:31 -0400

    update

--- projet_compass/routes/web.php
+++ projet_compass/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::get('Accueil', [StudentsController::class, 'index'])->name('etudiant.index');
