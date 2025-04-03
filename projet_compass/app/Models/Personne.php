844a5d6ce3be6aa33505b66d0e5094d0e3578f8b
sricher86 <sricher86@hotmail.com>
2025-04-03 09:28:18 -0400

    modele personne

--- projet_compass/app/Models/Personne.php
+++ projet_compass/app/Models/Personne.php
@@ -14,4 +14,36 @@ class Personne extends Model
         'age',
         'sexe'
     ];
+
+    protected $hidden = [
+        'id'
+    ];
+
+    public function disciplines () {
+        return $this ->hasMany(Discipline::class);
+    }
+
+    public function programmes () {
+        return $this ->hasMany(Programme::class);
+    }
+
+    public function langues () {
+        return $this ->hasMany(Langue::class);
+    }
+
+    public function interets () {
+        return $this ->hasMany(Interet::class);
+    }
+
+    public function disponibilites () {
+        return $this ->hasMany(Disponibilite::class);
+    }
+
+    public function rencontres () {
+        return $this ->hasMany(Rencontre::class);
+    }
+
+    public function hobbies () {
+        return $this ->hasMany(Hobby::class);
+    }
 }
