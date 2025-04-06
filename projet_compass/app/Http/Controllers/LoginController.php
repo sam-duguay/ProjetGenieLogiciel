174e7ae5d4eca550329edec3094f4a49f1b890f1
fantaMomo-cmr <fantamomo21@gmail.com>
2025-04-05 21:35:55 -0400

    login

--- projet_compass/app/Http/Controllers/LoginController.php
+++ projet_compass/app/Http/Controllers/LoginController.php
@@ -2,11 +2,41 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
 
 class LoginController extends Controller
 {
     public function getLoginForm() {
-        return view('register.register');
+        return view('login.login');
     }
+
+    public function login(LoginRequest $request)
+    {
+        // dd($request);
+        // try {
+            $user = User::query()->where('email',$request->email)->first();
+
+            if ($user && Hash::check($request->password, $user->password)) {
+               return redirect()->route('home')->with('success', 'Login successful');
+            } 
+            else {
+                return redirect()->back()->with('error', 'Invalid credentials');
+            }
+          
+        // } catch (\Exception $e) {
+        //     return response()->json([
+        //         'status' => 400,
+        //         'errors' => $e
+        //     ]);
+        // }
+    }
+   
+
+
 }
