6b6ec7130f2a5eac085a3433adcffb651c2b9e90
fantaMomo-cmr <fantamomo21@gmail.com>
2025-04-15 14:03:28 -0400

    rien

--- projet_compass/app/Http/Controllers/PersonnesController.php
+++ projet_compass/app/Http/Controllers/PersonnesController.php
@@ -17,53 +17,13 @@ class PersonnesController extends Controller
         $personne = Personne::find($id);
         return view('fillprofile.fillprofile', compact('id', 'disciplines','personne'));
     }
-    //code de base
-// dd($request);
-        // try{             
-           
-        //     $personne = Personne::find($id);
-
-        //     if($personne)
-        //     {
-        //         $personne->nom = $request->nom;
-        //         $personne->prenom = $request->prenom;
-        //         $personne->statut = $request->statut;
-        //         $personne->photo = $request->photo;
-        //         $personne->age = $request->age;
-        //         $personne->sexe  = $request->sexe;
-        //         $personne->discipline_id = $request->discipline_id;
-        //         // $personne->programme_id  = $request->programme_id;
-        //     }
-        //     $fichier = $request->file('photo');
-        //     $nomFichier = 'photo_' . uniqid() . '.' . $fichier->getClientOriginalExtension();
-
-        //     //$nomFichier = str_replace(' ', '_', $request->photo) . '-' . uniqid() . '-' . $fichier->extension(); 
-
-        //     try {
-        //         $request->photo->move(public_path('img/personnes'), $nomFichier);
-        //     }
-        //     catch(\Symfony\Component\HttpFoundation\File\Exception\FileException $e) {
-        //         Log::error('Erreur lors du téléversement du fichier. ', [$e]);
-        //     }
-
-        //     $personne->photo = $nomFichier;
-
-        //     $personne->save();
-        //     return redirect()->route('/')->with('message', 'Enregistrement réussi'. $personne->nom . "registered successfully");            
-        // }
-        // catch (\Throwable $e){
-        //     // dd($request);
-
-        //     // storage/logs
-        //     Log::debug($e);
-        // }
     public function update(PersonneRequest $request, Personne $personne, $id) 
     {
         
        $personne = Personne::find($id);
 
     //    try{
-        //if($personne) {
+        if($personne) {
             $personne->nom = $request->nom;
             $personne->prenom = $request->prenom;
             $personne->statut = $request->statut;
@@ -88,12 +48,12 @@ class PersonnesController extends Controller
             $personne->save();
     
             return redirect()->route('home')->with('message', 'Enregistrement réussi : ' . $personne->nom);
-            //}
+    }
 
     //    }catch (\Throwable $e){
 
     //    }
-       
+
 
     }
 
