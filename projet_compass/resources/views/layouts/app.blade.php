f6d9fcc883596e2d947c9bfee80da206eb8dee29
sricher86 <sricher86@hotmail.com>
2025-04-25 19:51:46 -0400

    profilmatch

--- projet_compass/resources/views/layouts/app.blade.php
+++ projet_compass/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr-ca">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -8,21 +9,21 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Rencontre+ - @yield('title')</title>
-    
+
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('asset/img/apple-icon.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('asset/img/favicon.png') }}">
-    
+
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,100..900;1,9..144,100..900&display=swap" rel="stylesheet">
-    
+
     <!-- Icons -->
     <link href="{{ asset('asset/css/nucleo-icons.css') }}" rel="stylesheet" />
-    
+
     <!-- CSS -->
     <link href="{{ asset('asset/css/black-dashboard.css?v=1.0.0') }}" rel="stylesheet" />
     <link href="{{ asset('asset/css/theme.css') }}" rel="stylesheet" />
@@ -31,136 +32,142 @@
 </head>
 
 
-    <body>
-        <div class="wrapper">
-            <!-- Sidebar -->
-            <!-- Elle s'affiche seulement si l'attribut show_sidebar est retourné à true dans le controller en question -->
-            @if(isset($show_sidebar) && $show_sidebar)
-            <div class="sidebar">
-                <div class="sidebar-wrapper">
-                    <div class="logo">
-                        <p class="simple-text">Rencontre +</p>
+<body>
+    <div class="wrapper">
+        <!-- Sidebar -->
+        <!-- Elle s'affiche seulement si l'attribut show_sidebar est retourné à true dans le controller en question -->
+        @if(isset($show_sidebar) && $show_sidebar)
+        <div class="sidebar">
+            <div class="sidebar-wrapper">
+                <div class="logo">
+                    <p class="simple-text">Rencontre +</p>
+                </div>
+                <br>
+
+                <ul class="nav">
+                    <li></li>
+                    <li>
+                        <a href="{{ route('suggestion.index') }}">
+                            <i class="fab fa-laravel"></i>
+                            <span class="nav-link-text">Suggestions</span>
+                        </a>
+                    </li>
+                    <li>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+        @endif
+
+
+
+        <!-- Le main panel qui contiendra la navbar -->
+        <div class="main-panel">
+
+            <!-- La navbar -->
+            <nav class="navbar navbar-expand-lg navbar-absolute navbar-transparent">
+
+                <div class="container-fluid">
+
+                    <div class="navbar-wrapper">
+                        <div class="navbar-toggle d-inline">
+                            <button type="button" class="navbar-toggler">
+                                <span class="navbar-toggler-bar bar1"></span>
+                                <span class="navbar-toggler-bar bar2"></span>
+                                <span class="navbar-toggler-bar bar3"></span>
+                            </button>
+
+                        </div>
+
+                        <!-- <a class="navbar-brand" href="/">Accueil</a> -->
                     </div>
-                    <br>
-                
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-bar navbar-kebab"></span>
+                        <span class="navbar-toggler-bar navbar-kebab"></span>
+                        <span class="navbar-toggler-bar navbar-kebab"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navigation">
+                        <ul class="navbar-nav ml-auto">
+
+                            @if(Auth::check())
+                            <li class="nav-item">
+                                <a class="nav-link {{ Request::is( 'home') ? 'active' : ''}}" href=" {{ route('home') }}">
+                                    Accueil
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link {{ Request::is( 'fillprofile') ? 'active' : ''}}" href="{{ 'fillprofile/' . Auth::user()->personne->id }}">
+                                    Mettre à jour profile
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link {{ Request::is( 'logout') ? 'active' : ''}}" href="{{ route('logout') }}">
+                                    Déconnexion
+                                </a>
+                            </li>
+
+
+                            @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">
+                                    Connexion
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('inscription') }}">
+                                    Se créer un compte
+                                </a>
+                            </li>
+                            @endif
+
+                            <li class="separator d-lg-none"></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="content container-fluid {{ isset($show_sidebar) && $show_sidebar ? '' : 'px-0' }}">
+                @yield('content')
+            </div>
+
+
+            <!-- Le footer -->
+
+            <footer class="footer">
+                <div class="container-fluid">
                     <ul class="nav">
-                        <li></li>
-                        <li>
-                            <a href="{{ route('suggestion.index') }}">
-                                <i class="fab fa-laravel"></i>
-                                <span class="nav-link-text">Suggestions</span>
+                        <li class="nav-item">
+                            <a href="https://creative-tim.com" target="blank" class="nav-link">
+                                Creative Tim
                             </a>
                         </li>
-                        <li>
-                            
+                        <li class="nav-item">
+                            <a href="https://updivision.com" target="blank" class="nav-link">
+                                Updivision
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                About Us
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                Blog
+                            </a>
                         </li>
                     </ul>
                 </div>
-            </div>
-            @endif
-
-
-
-            <!-- Le main panel qui contiendra la navbar -->
-            <div class="main-panel">
-
-                <!-- La navbar -->
-                <nav class="navbar navbar-expand-lg navbar-absolute navbar-transparent">
-                
-                    <div class="container-fluid">
-
-                        <div class="navbar-wrapper">
-                            <div class="navbar-toggle d-inline">
-                                <button type="button" class="navbar-toggler">
-                                    <span class="navbar-toggler-bar bar1"></span>
-                                    <span class="navbar-toggler-bar bar2"></span>
-                                    <span class="navbar-toggler-bar bar3"></span>
-                                </button>
-
-                            </div>
-
-                            <a class="navbar-brand" href="/">Accueil</a>
-                        </div>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                            <span class="navbar-toggler-bar navbar-kebab"></span>
-                            <span class="navbar-toggler-bar navbar-kebab"></span>
-                            <span class="navbar-toggler-bar navbar-kebab"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navigation">
-                            <ul class="navbar-nav ml-auto">
-
-                            @if(Auth::check())
-                                <li class="nav-item">
-                                    <a class="nav-link {{ Request::is( 'logout') ? 'active' : ''}}" href="{{ 'fillprofile/' . Auth::user()->personne->id }}">
-                                    Mettre à jour profile
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link {{ Request::is( 'logout') ? 'active' : ''}}" href="{{ route('logout') }}">
-                                    Déconnexion
-                                    </a>
-                                </li>
-
-                               
-                                @else
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">
-                                    Connexion
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('inscription') }}">
-                                    Se créer un compte
-                                    </a>
-                                </li>
-                            @endif
-
-                                <li class="separator d-lg-none"></li>
-                                
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-
-                <div class="content container-fluid {{ isset($show_sidebar) && $show_sidebar ? '' : 'px-0' }}">
-                    @yield('content')
-                </div>
-
-
-                <!-- Le footer -->
-
-                <footer class="footer">
-                    <div class="container-fluid">
-                        <ul class="nav">
-                            <li class="nav-item">
-                                <a href="https://creative-tim.com" target="blank" class="nav-link">
-                                    Creative Tim
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://updivision.com" target="blank" class="nav-link">
-                                    Updivision
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    About Us
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    Blog
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </footer>
-
-            </div>
+            </footer>
 
         </div>
 
-    </body>
-    @stack('js')
-    @stack('scripts')
+    </div>
+
+</body>
+@stack('js')
+@stack('scripts')
+
 </html>
--- projet_compass/resources/views/profilpersonne/profilpersonne.blade.php
+++ projet_compass/resources/views/profilpersonne/profilpersonne.blade.php
@@ -12,78 +12,134 @@
 
 @if(Auth::check())
 @section('content')
-<section>
-    <div class="container-fluid w-50 pb-5">
-        <div class="row align-items-center">
-            <div class="col-8">
-                <div class="row">
-                    <h3 class="nom">{{ $match->nom . ' ' . $match->prenom }}</h3>
+<div class="container-fluid pb-5 mx-2 me-5">
+    <div class="row ps-3">
+        <div class="col-6 ps-5">
+            <div class="row align-items-center ps-5">
+                <div class="col-4">
+                    <img src="{{ asset($match->photo) }}" class="img-profil" alt="">
                 </div>
-                <div class="row">
-                    <p class="bio-texte"> {{ $match->bio->bio }}</p>
+                <div class="col-8 ps-3">
+                    <div class="row">
+                        <h3 class="nom">{{ $match->prenom . ' ' . $match->nom }}</h3>
+                    </div>
+                    <div class="row">
+                        <p class="bio-texte"> {{ $match->bio->bio }}</p>
+                    </div>
                 </div>
             </div>
-            <div class="col-4">
-                <img src="{{ asset($match->photo) }}" class="img-profil" alt="">
+            <div class="row ml-1 mt-1">
+                <div class="col-7 conteneur-interet">
+                    <div class="row">
+                        <h2 class="pt-4">Intérêts</h2>
+                    </div>
+                    <div class="row">
+                        @foreach ($match->interets as $interet)
+                        <div class="overlay-container w-25">
+                            <img src="{{ asset($interet->photo) }}" class="img-interet" alt="">
+                            <div class="overlay">
+                                <div class="overlay-txt">{{$interet->nom}}</h4>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="col-5 conteneur-hobbies">
+                    <div class="row">
+                        <h2 class="pt-4">Hobbies</h2>
+                    </div>
+                    <div class="row">
+                        @foreach ($match->hobbies as $hobby)
+                        <div class="overlay-container w-25">
+                            <!-- <img src="{{ asset($hobby->photo) }}" class="img-interet" alt=""> -->
+                            <div class="overlay">
+                                <div class="overlay-txt">{{$hobby->nom}}</div>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <h2 class="sous-titre-profil pt-4">Intérêts</h2>
-        </div>
-        <div class="row">
-            <div class="offset-4 col-8">
-                <h2 class="texte-profil pt-4">Intérêts</h2>
-            </div>
-        </div>
+        <div id="myModal" class="modal">
 
-    </div>
-</section>
-<section>
-    <div class="container w-50 mx-auto">
-        <h1 class="titre-calendrier">Disponibilités</h1>
-        <div class="row w-100">
-            <div id="calendar">
-                @push('scripts')
-                <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/core/main.css" rel="stylesheet" />
-                <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid/main.css" rel="stylesheet" />
-                <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid/main.css" rel="stylesheet" />
-                <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
-                <script src='fullcalendar/dist/index.global.js'></script>
-                <script>
-                    document.addEventListener('DOMContentLoaded', function() {
-                        var calendarEl = document.getElementById('calendar');
-                        var calendar = new FullCalendar.Calendar(calendarEl, {
-                            // dateClick: function() {
-                            //     alert('a date has been clicked');
-                            // },
-                            eventClick: function(info) {
-                                info.jsEvent.preventDefault(); // don't let the browser navigate
-                                console.log(info.event.url)
-                                if (info.event.url) {
-                                    window.open(info.event.url + '/' + info.event.id);
+            <!-- Modal content -->
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <p>Some text in the Modal..</p>
+            </div>
+
+        </div>
+        <div class="col-6 me-1 pe-1">
+            <div class="container mx-auto conteneur-calendrier">
+                <!-- <h2 class="titre-calendrier text-start">Disponibilités</h2> -->
+                <div class="row w-100">
+                    <div id="calendar">
+                        @push('scripts')
+                        <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/core/main.css" rel="stylesheet" />
+                        <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid/main.css" rel="stylesheet" />
+                        <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid/main.css" rel="stylesheet" />
+                        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
+                        <script src='fullcalendar/dist/index.global.js'></script>
+                        <script>
+                            // Get the modal
+                            var modal = document.getElementById("myModal");
+
+                            // Get the <span> element that closes the modal
+                            var span = document.getElementsByClassName("close")[0];
+
+                            // When the user clicks on <span> (x), close the modal
+                            span.onclick = function() {
+                                modal.style.display = "none";
+                            }
+
+                            // When the user clicks anywhere outside of the modal, close it
+                            window.onclick = function(event) {
+                                if (event.target == modal) {
+                                    modal.style.display = "none";
                                 }
-                                // alert('Event: ' + info.event.startdate + '\n\nVoulez vous organiser une rencontre à cette date?');
-                                // // change the border color just for fun
-                                // info.el.style.borderColor = 'red';
+                            }
 
-                            },
-                            initialView: 'timeGridWeek',
-                            slotMinTime: '8:00:00',
-                            slotMaxTime: '21:00:00',
-                            events: @json($events),
-                            editable: true
-                        });
-                        calendar.render();
-                        calendar.setOption('height', "auto");
-                        calendar.editable = true
-                    });
-                </script>
-                @endpush
+                            document.addEventListener('DOMContentLoaded', function() {
+                                var calendarEl = document.getElementById('calendar');
+                                var calendar = new FullCalendar.Calendar(calendarEl, {
+                                    // dateClick: function() {
+                                    //     alert('a date has been clicked');
+                                    // },
+                                    eventClick: function(info) {
+                                        info.jsEvent.preventDefault(); // don't let the browser navigate
+
+                                        modal.style.display = "block";
+
+                                        console.log(info.event.url)
+                                        // if (info.event.url) {
+                                        //     window.open(info.event.url + '/' + info.event.id);
+                                        // }
+                                        // alert('Event: ' + info.event.startdate + '\n\nVoulez vous organiser une rencontre à cette date?');
+                                        // // change the border color just for fun
+                                        // info.el.style.borderColor = 'red';
+
+                                    },
+                                    initialView: 'timeGridWeek',
+                                    slotMinTime: '8:00:00',
+                                    slotMaxTime: '21:00:00',
+                                    events: @json($events),
+                                    editable: true
+                                });
+                                calendar.render();
+                                calendar.setOption('height', "auto");
+                                calendar.editable = true
+                            });
+                        </script>
+                        @endpush
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
-</section>
+</div>
+</div>
 @endsection
 @else
 <div class="card">
